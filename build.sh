1fcb00594a14a304b16be5b6b95277712fa4f208
Dániel Lengyel <daniel.lengyel@sophos.com>
2022-05-18 15:28:31 +0200

    Even more cleaning

--- build.sh
+++ build.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+set -e
+
+# Install zlog
+cd src
+wget https://github.com/HardySimpson/zlog/archive/refs/tags/1.2.15.tar.gz
+tar -zxvf 1.2.15.tar.gz
+rm 1.2.15.tar.gz
+cd zlog-1.2.15
+make
+make install
+cd ../..
+
+# Build project
 mkdir build
 cd build
 cmake ..
